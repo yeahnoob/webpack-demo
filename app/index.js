052780ec1b5fb87d4b1e045d50b6bcd74ea3e333
yeahnoob <yeahnoob@buaa.edu.cn>
2017-03-09 15:20:05 +0800

    trying the examples in the Book , chapter 2

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
-import {join as strJoin} from 'lodash/fp';
+import {join as strJoin} from 'lodash/fp'
+import {learning_react_ch2} from './learning_react'
 
-function component () {
-    var element = document.createElement('div');
-    element.innerHTML = strJoin(' ')(['Good', 'Morning', 'webpack']);
-    return element;
+function component() {
+    var element = document.createElement('div')
+    element.innerHTML = strJoin(' ')(['Good', 'Morning!', ' & Let\'s use webpack'])
+    learning_react_ch2()
+    return element
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component())
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 var path = require('path');
 
 module.exports = {
-  entry: './app/index.js',
+  entry: {
+    main: './app/index.js',
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: 'dist'
+  },
+  module: {
+    rules: [
+    { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+    ]
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, "app")
+    ],
+    extensions: ['.js', '.json', '.jsx', '.xcc'],
   }
 };
