74992d7ba61450283a853f7689c33868e1396252
yeahnoob <yeahnoob@buaa.edu.cn>
2017-03-09 15:21:02 +0800

    chapter 2 examples

---
+++ app/learning_react.js
@@ -0,0 +1,12 @@
+module.exports = {
+    learning_react_ch2: book_ch2_1
+}
+
+function book_ch2_1 () {
+    var topic = 'globale_mark'
+    if (topic) {
+        let topic = 'inblock_mark'
+        console.log('inblock value:', topic)
+    }
+    console.log('global value:', topic)
+}
